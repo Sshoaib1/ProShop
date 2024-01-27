98bc7ca80b9f1d7235c8f2055285fc85bee4798f
M Shoaib Rizwan <muhammadshoaibrizwan@gmail.com>
2024-01-27 19:04:24 +0500

    Added react-toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Header from './components/Header'
-import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-         <Outlet/>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
+      <ToastContainer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
