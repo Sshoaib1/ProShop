102d64d562fb9b4eda622de89ffd0fc04b07698f
M Shoaib Rizwan <muhammadshoaibrizwan@gmail.com>
2024-02-05 19:00:58 +0500

    Products List fro Admin

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -78,7 +78,7 @@ const Header = () => {
               )}
                 {userInfo && userInfo.isAdmin && (
                   <NavDropdown title="Admin" id="adminmenu">
-                    <LinkContainer to="/admin/produtclist">
+                    <LinkContainer to="/admin/productlist">
                       <NavDropdown.Item>Products</NavDropdown.Item>
                     </LinkContainer>
                     <LinkContainer to="/admin/orderlist">
