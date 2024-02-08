3b47903bc834b5624a424cf8ac5e1b6c98dfb0ac
M Shoaib Rizwan <muhammadshoaibrizwan@gmail.com>
2024-02-08 19:07:43 +0500

    Edit Product Details Functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -40,9 +40,35 @@ const createProduct = asyncHandler(async (req, res) => {
     numReviews: 0,
     description: "Sample description",
   });
-  
+
   const createdProduct = await products.save();
   res.status(200).json(createdProduct);
 });
 
-export { getProducts, getProductById, createProduct };
+// @desc    Update a Product
+// @route   PUT /api/products/:id
+// @access  Private/admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updateProduct = await product.save();
+    res.jason(updateProduct);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+export { getProducts, getProductById, createProduct, updateProduct };
