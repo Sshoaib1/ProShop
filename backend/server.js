b5c82f36d9ceb8f0ace7413a7bcd68e76bdcba9a
M Shoaib Rizwan <muhammadshoaibrizwan@gmail.com>
2024-02-08 19:35:39 +0500

    Multer and Image Upload Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -6,6 +7,7 @@ dotenv.config();
 import producRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 // const express=require('express'); we can use this when we will not use es module
 const port = process.env.PORT || 5000;
@@ -27,12 +29,14 @@ app.get("/", (req, res) => {
 app.use("/api/products", producRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
-// Example route for updating payment status
-
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+const __dirname = path.resolve(); // set dir to current dir
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
